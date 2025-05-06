8acf582588fed85c4f9feb835ac2a7ec05412044
Tejas-Joshi-17 <joshitejas188@gmail.com>
2025-05-06 22:44:39 +0530

    initial commit

---
+++ src/main/java/com/sarvatra/Main.java
@@ -0,0 +1,13 @@
+package com.sarvatra;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    protected static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("All Working Fine");
+    }
+}
